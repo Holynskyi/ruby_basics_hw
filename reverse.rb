5caf5b57462491415392a6cb88a4285c2183d8cb
Holynskyi <Leviafan1@i.ua>
2016-05-01 23:09:33 +0300

    add alt version

--- reverse.rb
+++ reverse.rb
@@ -13,8 +13,22 @@ def my_reverse(arr)
 	return hytro_reverse(m)
 end
 
+def alt_reverse(initial_array,reverse_array=initial_array,i=0)
+	if i==0
+		reverse_array=initial_array.dup
+	end
+	reverse_array[i]=initial_array[-i-1]
+	i=i+1
+	if i>=initial_array.length
+		return reverse_array
+	else
+		reverse_array=alt_reverse(initial_array,reverse_array,i)
+	end
+
+end
+
 input=[0,1,"dva",3.14,4,5,6,7.0]
-output=my_reverse (input)
+output=alt_reverse (input)
 
 puts input
 puts"---------"
